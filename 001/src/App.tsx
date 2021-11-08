516f17288c822f3f342199d15bf17e08bd38bb56
Irina Novakovich <irinaredhood@gmail.com>
2021-11-08 15:38:54 +0300

    Made new changes on HW2. wrote components and followed them on console.log

--- 001/src/App.tsx
+++ 001/src/App.tsx
@@ -3,45 +3,68 @@ import './App.css';
 
 //function declaration
 function App() {
-    debugger
     //function does smth useful
     //must return JSX
-  return (
-    <div>
-      This is APP component
-        <Rating />
-        <Accordion />
-    </div>
-  );
+    console.log("App rendering")
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
+}
+
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return <>This is APP component</>
 }
 
 function Rating() {
+    console.log("Rating rendering")
     return (
-        <div>
+        <>
             <Star/>
             <Star/>
             <Star/>
             <Star/>
             <Star/>
-        </div>
+        </>
     )
 }
 
 function Star() {
+    console.log("Star rendering")
     return <div>
         Star
     </div>
 }
 
 function Accordion() {
+    console.log("Accordion rendering")
     return <div>
-        <h3>Menu</h3>
+        <AccordionTitle/>
+        <AccordionBody/>
+
+
+    </div>
+}
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return <><h3>Menu</h3></>
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return <>
         <ul>
             <li>1</li>
             <li>2</li>
             <li>3</li>
         </ul>
-    </div>
+    </>
 }
 
 export default App;
