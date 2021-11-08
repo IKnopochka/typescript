083d861ced7eef025851f9084d7e3dfe4231a863
Irina Novakovich <irinaredhood@gmail.com>
2021-11-08 19:41:04 +0300

    types and values

--- 001/src/components/rating/Rating.tsx
+++ 001/src/components/rating/Rating.tsx
@@ -3,19 +3,17 @@ import React from "react";
 export function Rating() {
     console.log("Rating rendering")
     return (
-        <>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </>
+        <div>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
     )
 }
 
-function Star() {
+function Star(props: any) {
     console.log("Star rendering")
-    return <div>
-        Star
-    </div>
+    return <span><b>Star </b></span>
 }
