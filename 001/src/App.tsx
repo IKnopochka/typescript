5ddb08785e31109f10059331de2e340422ee0007
Irina Novakovich <irinaredhood@gmail.com>
2021-11-08 15:57:21 +0300

    created new folders and learning import/export of components

--- 001/src/App.tsx
+++ 001/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Accordion from './components/accordion/Accordion';
 
 //function declaration
 function App() {
@@ -39,32 +40,6 @@ function Star() {
     return <div>
         Star
     </div>
-}
-
-function Accordion() {
-    console.log("Accordion rendering")
-    return <div>
-        <AccordionTitle/>
-        <AccordionBody/>
-
-
-    </div>
-}
-
-function AccordionTitle() {
-    console.log("AccordionTitle rendering")
-    return <><h3>Menu</h3></>
-}
-
-function AccordionBody() {
-    console.log("AccordionBody rendering")
-    return <>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    </>
 }
 
 export default App;
