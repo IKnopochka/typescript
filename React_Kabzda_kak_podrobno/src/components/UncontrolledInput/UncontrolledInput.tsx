46be0ab243a0d5e7ba28730dfad3340d0872079c
Irina Novakovich <irinaredhood@gmail.com>
2022-05-13 21:44:02 +0300

    React-kabzda-kak-podrobno 12-13

---
+++ React_Kabzda_kak_podrobno/src/components/UncontrolledInput/UncontrolledInput.tsx
@@ -0,0 +1,42 @@
+import React, {useRef, useState} from 'react';
+
+
+export const UncontrolledInput = () => <input/>
+
+export const TrackValueOfUncontrolledInput = () => {
+    const[value, setValue] = useState('')
+    return <><input onChange={(e) =>{
+        setValue(e.currentTarget.value)
+    } }/> - {value}</>
+}
+
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+
+    const[value, setValue] = useState('')
+
+    const inputRef = useRef<HTMLInputElement>(null)
+    const addValue = () => {
+        const el = inputRef.current as HTMLInputElement
+            setValue(el.value)
+    }
+
+    return <> <input ref={inputRef}/>
+        <button onClick={addValue}>save</button> - actual value: {value}
+    </>
+}
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    return <input value={parentValue} onChange={(e) => {setParentValue(e.currentTarget.value)}}/>
+}
+export const ControlledCheckBox = () => {
+    const [parentValue, setParentValue] = useState(false)
+    return <div>
+        <input type={"checkbox"}
+               checked={parentValue}
+               onChange={(e) => {setParentValue(e.currentTarget.checked)}}/>
+    </div>
+}
+export const ControlledSelect = () => {
+
+}
+
