3ef507eeabe3bf63469ac1844c1a8ad83a43cc5a
Irina Novakovich <irinaredhood@gmail.com>
2022-10-08 23:20:38 +0300

    React-kabzda-kak-podrobno 21 Lesson  Lesson useEffect

--- React_Kabzda_kak_podrobno/src/stories/UseEffect.stories.tsx
+++ React_Kabzda_kak_podrobno/src/stories/UseEffect.stories.tsx
@@ -13,21 +13,69 @@ export const SimpleExample = () => {
 
     useEffect(() => {
         console.log('UseEffect renders every time')
-
+        document.title = String((counter))
     })
+
     useEffect(() => {
         console.log('UseEffect renders once')
-
-
+        document.title = String((counter))
     }, [])
+
     useEffect(() => {
         console.log('UseEffect renders when counter changes')
+        document.title = String((counter))
+    }, [counter])
+
+    return <>
+        Hello, {counter}
+        <button onClick={() => setCounter(counter + 1)}>Add Count</button>
+    </>
+}
+
+export const SetTimeoutExample = () => {
+    console.log('setTimeout Example')
+
+    const [counter, setCounter] = useState(0)
+    const [fake, setFake] = useState(0)
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            console.log('SetTimeout')
+            document.title = String((counter))
+        }, 2000)
 
 
     }, [counter])
 
     return <>
         Hello, {counter}
-        <button onClick={() => setCounter(counter + 1)}>Add Count</button>
+        <button onClick={() => setCounter(counter + 1)}>Count+</button>
+        <button onClick={() => setCounter(fake + 1)}>Fake+</button>
+    </>
+}
+
+export const SetIntervalExample = () => {
+    console.log('Interval Example')
+
+    const [counter, setCounter] = useState(0)
+    const [fake, setFake] = useState(0)
+
+    useEffect(() => {
+
+        setInterval(() => {
+            console.log('tick ' + 1)
+            setCounter(state => state + 1)
+        }, 2000)
+
+
+    }, [])
+
+    return <>
+        Hello,
+        fake: {fake}
+        counter: {counter}
+        {/*<button onClick={() => setCounter(counter + 1)}> Count+ </button>
+        <button onClick={() => setFake(fake + 1)}> Fake+ </button>*/}
     </>
 }
